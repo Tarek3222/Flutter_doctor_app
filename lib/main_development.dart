4103d87ef46920b97e92fdf001a144fed5373672
Tarek3222 <118009219+Tarek3222@users.noreply.github.com>
2025-02-15 15:30:46 +0200

    - added Development & Production flavors for android - added launch.json for flavors config. - changed Android app name for each flavor - added two main files for each flavors

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,9 +2,12 @@ import 'package:doctor/core/di/dependancy_injection.dart';
 import 'package:doctor/core/routing/app_router.dart';
 import 'package:doctor/doctor_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   setupGetIt();
+  // to fix text being hidden due to bug in flutter_screenutil package in release mode
+  await ScreenUtil.ensureScreenSize();
   runApp(
     DoctorApp(
       appRouter: AppRouter(),
